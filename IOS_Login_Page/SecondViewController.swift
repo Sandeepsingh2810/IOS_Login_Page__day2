bf733874de397895b38a450aaf87156d6d5f40ef
Karan Kumar <kumarkay.ca@gmail..com>
2019-10-28 18:20:37 -0400

    Second Screen with Navigation Controller

---
+++ IOS_Login_Page/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  IOS_Login_Page
+//
+//  Created by MacStudent on 2019-10-28.
+//  Copyright © 2019 Karan Kumar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+}
